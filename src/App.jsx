e6eb777c856cb5178a3aecbee7d21d0c92940768
Tiago <tiagodev@protonmail.com>
2023-02-15 20:16:54 -0300

    getting all the data

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,39 @@ import { useEffect, useState } from "react";
 import { fetchData } from "./fetchData";
 
 function App() {
-  const [games, setGames] = useState(null);
-
+  const [games, setGames] = useState([]);
+  
   useEffect(() => {
     async function getData() {
       const data = await fetchData();
-      setGames(data)
+      setGames(data);
     }
-    
-    if(games == null) {
+
+    if (games.length === 0) {
       getData();
     }
   }, [games]);
-
-  return <h1>{games ? games[0].name : 'Loading...'}</h1>;
+  console.log('JOQUINHOS' ,games);
+  return (
+    <>
+      {games.map((item, index) => {
+        console.log('CADA IMAGEM',item.image)
+        
+        return (
+          <div key={item.id}>
+            <h3>{item.name ? item.name : "Loading..."}</h3>
+            <img
+              style={{ width: 100 + "px" }}
+              src={`/src/assets/img/${item.image}`}
+              alt={`${item.name}`}
+            />
+            <h4>Price: ${item.price}</h4>
+            <h5>Price: ${item.score}</h5>
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default App;
